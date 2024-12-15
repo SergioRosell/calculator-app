6b592d4a82a8dfe155afe77240564da71fc38751
SergioRosell <sergio.rosell.dev@gmail.com>
2024-12-15 23:34:20 +0100

    end iOS calculator

--- src/app/main-calculator/main-calculator.component.ts
+++ src/app/main-calculator/main-calculator.component.ts
@@ -1,12 +1,40 @@
+import { evaluate } from 'mathjs';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-main-calculator',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './main-calculator.component.html',
   styleUrl: './main-calculator.component.scss'
 })
 export class MainCalculatorComponent {
+
+   calculatorInput:string = '0';
+
+  appendToDisplay(symbol:string):void{
+    if (this.calculatorInput === '0') {
+      this.calculatorInput = symbol;
+    } else {
+      this.calculatorInput += symbol;
+    }
+  }
+
+  calculation(){
+    try {
+      this.calculatorInput = evaluate(this.calculatorInput).toString();
+    } catch (error) {
+      this.calculatorInput = "Error";
+    }
+  }
+
+  deleteDisplay(){
+    if (this.calculatorInput.length === 1) {
+      this.calculatorInput = '0';
+    } else {
+      this.calculatorInput = this.calculatorInput.slice(0,-1);
+    }
+  }
 
 }
